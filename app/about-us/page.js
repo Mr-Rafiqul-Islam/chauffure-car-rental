aaf0a35be0ed963fb4fd1b4be006f3bb1f297831
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-09-25 13:33:11 +0600

    Feat: About Page complete

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -1,4 +1,6 @@
 
+import TestimonialsCarousel from "@/components/home/TestimonialsCarousel";
+import AboutUs from "@/components/mvpblocks/about-us-1";
 import PageHeader from "@/components/ui/lamp";
 import React from "react";
 
@@ -6,6 +8,8 @@ const page = () => {
   return (
     <div>
       <PageHeader pageName="About Us" subheading="Melbourne’s Trusted Name in Chauffeur Services" />
+      <AboutUs/>
+      <TestimonialsCarousel/>
     </div>
   );
 };
