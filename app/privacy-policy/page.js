5f8c978299b399fe0731e0fcd25e8c296f8165ce
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-10-07 04:05:07 +0600

    Feat: Two page Added

---
+++ app/privacy-policy/page.js
@@ -0,0 +1,14 @@
+import MaintenancePage from "@/components/common/MaintenancePage";
+import React from "react";
+export const metadata = {
+  title: "Privacy Policy | Luxury Chaffure",
+};
+const page = () => {
+  return (
+    <div>
+        <MaintenancePage pageName="Privacy Policy" />
+    </div>
+  );
+};
+
+export default page;
---
+++ app/terms-and-conditions/page.js
@@ -0,0 +1,14 @@
+import MaintenancePage from '@/components/common/MaintenancePage';
+import React from 'react'
+export const metadata = {
+  title: "Terms & Conditions | Luxury Chaffure",
+};
+const page = () => {
+  return (
+    <div>
+        <MaintenancePage pageName="Terms & Conditions" />
+    </div>
+  )
+}
+
+export default page
