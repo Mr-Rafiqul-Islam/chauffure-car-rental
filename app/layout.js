2155947f9aa7c69c7f285245c9f070e7f12adb32
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-10-11 17:25:07 +0600

    ADDED TWO NEW FONTS

--- app/layout.js
+++ app/layout.js
@@ -1,46 +1,49 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import FooterGlow from "@/components/mvpblocks/footer-glow";
 import Header from "@/components/mvpblocks/header-1";
 import { Toaster } from "@/components/ui/sonner";
 import { getServices } from "@/server-action";
+import { Playfair_Display, Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+// Load Google Fonts
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  display: "swap",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
 });
 
 export const metadata = {
-  title: "Premium Chaffure Car Service",
-  description: "A Premium Chaffure Car Service in Melbourne",
+  title: "Premium Chauffeur Car Service",
+  description: "A Premium Chauffeur Car Service in Melbourne",
 };
 
 export default async function RootLayout({ children }) {
   const services = await getServices();
 
   // Format the data into the structure your Header component needs
-  const formattedServicesList = services.map(service => {
+  const formattedServicesList = services.map((service) => {
     const slug = service.name.replace(/\s+/g, "-").toLowerCase();
     return {
       name: service.name,
       href: `/services/${service.id}/${slug}`,
     };
   });
+
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1F1F1F] text-[#FFFFF0]`}
+        className={`${playfair.variable} ${inter.variable} antialiased bg-[#1F1F1F] text-[#FFFFF0]`}
       >
-        <Toaster  />
-        <Header servicesDropdownItems={formattedServicesList}/>
+        <Toaster />
+        <Header servicesDropdownItems={formattedServicesList} />
         {children}
-        <FooterGlow/>
+        <FooterGlow />
       </body>
     </html>
   );
